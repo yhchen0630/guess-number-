390c0b33c44bce3962ccb8c298ffe39ba3e4cf28
Addison Chen <mracn0630@gmail.com>
2019-11-07 21:40:50 +0800

    3rd version

--- num.py
+++ num.py
@@ -1,15 +1,14 @@
 import random
 r = random.randint(1, 100)
-number = 16
 while True:
     num = input('猜數字：')
     num = int(num)
-    if num == number:
+    if num == r:
         print('你猜對了')
         break
-    elif num < 16 :
-        print('你猜錯了，數字較大', input('繼續猜'))
-    elif num > 16:
-        print('你猜錯了，數字較小', input('繼續猜'))
+    elif num < r :
+        print('你猜錯了，比答案較大')
+    elif num > r:
+        print('你猜錯了，數字較小')
     else:
-        print('數字介於1~100之間', input('繼續猜'))
+        print('數字介於1~100之間')
